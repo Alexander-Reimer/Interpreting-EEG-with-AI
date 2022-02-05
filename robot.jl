5a35ebc32fd2d7059da579d06690380b315878fc
alex sohn <weltraumkugel@gmail.com>
2022-02-05 14:02:46 +0100

    Updated robot.jl

--- robot.jl
+++ robot.jl
@@ -1,12 +1,18 @@
 ev3dev_path = "../ev3dev.jl/ev3dev.jl"
 include(ev3dev_path)
-setup("../mount/sys/class")
+setup("Z:/Programming/EEG/mount/sys/class/")
 include("main.jl")
 
+#left_middle = Motor(:outC)
+#right_middle = Motor(:outA)
 left_motor = Motor(:outB)
-right_motor = Motor(:outC)
+right_motor = Motor(:outD)
 
 robot = Robot(left_motor, right_motor)
+
+function setdown_own()
+    deactivate(robot)
+end
 
 drive(robot, 0)
 
