7c0bdb1cfc1e03dc5e5fbf552ff43866eeb54808
alex sohn <weltraumkugel@gmail.com>
2022-02-18 17:12:03 +0100

    Created plot for demonstrating FFT & IFFT

--- paper/pictures/julia-plots/fft_plots.jl
+++ paper/pictures/julia-plots/fft_plots.jl
@@ -6,32 +6,35 @@ cd(cur_path)
 using PyPlot
 using FFTW
 
-#=
-function add_frequency!(x, y, frq)
-    y .+= sin.(2π * x * frq)
+
+function add_frequency!(x, y, frq, amplitude = 1)
+    y .+= amplitude .* sin.(2π * x * frq)
 end
 
-function frequency(len, sample_rate, freqs)
+function frequency(len, sample_rate, freqs, amplitudes = nothing)
+    if amplitudes === nothing
+        fill(1, length(freqs))
+    end
     stepsize = 1 / sample_rate
     x = [i for i = 0:stepsize:len]
     y = zeros(length(x))
-    for frq in freqs
-        add_frequency!(x, y, frq)
+    for i = 1:length(freqs)
+        add_frequency!(x, y, freqs[i], amplitudes[i])
     end
     return x, y
 end
-=#
+
 
 function get_fft(x, y; time = nothing)
     if time === nothing
         if x === nothing
-            @error "Neither x not time specified!"
+            @error "Neither x nor time specified!"
         else
             time = x[end]
         end
     end
     x_fft = [i for i = 0.0:length(y)-1]
-    y_fft = abs.(fft(y))
+    y_fft = (fft(y))
 
     # Remove Mirror Effect
     len_fft = length(y_fft)
@@ -62,8 +65,9 @@ end
 =#
 
 function fft_eeg(eeg_data, time)
-    x_fft, y_fft = get_fft(nothing, eeg_data, time = time)
-    return x_fft, y_fft
+    #x_fft, y_fft = get_fft(nothing, eeg_data, time = time)
+    y_fft = rfft(eeg_data)
+    return y_fft
 end
 
 function divide_into_channels(data, channels)
@@ -135,3 +139,75 @@ figure("Drei Sekunden, FFT")
 x_f, y_f = get_fft(y)
 plot(x_f, y_f)
 =#
+
+function get_eeg_data(path, data_x, data_y, endings, output)
+    sample_number = 1
+    while isfile(path * string(sample_number) * ".csv")
+        # Read recorded EEG data
+        sample_data_x = BrainFlow.read_file(path * string(sample_number) * ".csv")
+        # Remove 3rd and 4th channel as they are a lot worse than 1st and 2nd and aren't necessary
+        sample_data_x = sample_data_x[:, 1:2]
+        # Filter 50 Hz frequencies to remove environmental noise
+        for i = 1:size(sample_data_x)[2]
+            BrainFlow.remove_environmental_noise(view(sample_data_x, :, i), 200, BrainFlow.FIFTY)
+        end
+
+        sample_data_x = reshape(sample_data_x, (:, 1))
+        #sample_data_x[800] = endings[1][sample_number]
+
+        # Perform FFT on data, once per channel
+        #sample_data_x = [make_fft(sample_data_x[1:200])..., make_fft(sample_data_x[201:400])...]
+        # Append to existing data
+        data_x = [data_x sample_data_x]
+        sample_number += 1
+    end
+
+    # Append given "output" to data_y ([1.0, 0.0] for Blink and [0.0, 1.0] for NoBlink)
+    for i = 1:sample_number-1
+        data_y = [data_y output]
+    end
+
+    return data_x, data_y
+end
+
+function own_ifft(x_fft, y_fft)
+    
+end
+
+function blink_fft()
+    path = "C:/Users/alexs/OneDrive/Dokumente/Programming/Interpreting-EEG-with-AI/Blink/first_samples-before_01-15-2022/"
+    global data_x = Array{Float64}(undef, 400, 0)
+    global data_y = Array{Float64}(undef, 2, 0)
+    data_x, data_y = get_eeg_data(path, data_x, data_y, nothing, [1, 0])
+
+    fig = figure("Blink FFT")
+    plt
+
+    current_d = data_x[:, 1]
+    current_d = divide_into_channels(current_d, 2)
+
+    ax1 = plt.subplot(131, title = "Originale EEG Daten")
+    xlabel("Zeit in 5ms")
+    ylabel("Spannungsdifferenz in Mikrovolt")
+    #ax1.plot(current_d[1], color = "red")
+    ax1.plot(current_d[2])
+
+    ax2 = plt.subplot(132, title = "FFT (Spektralanalyse)")
+    xlabel("Frequenz in Hertz")
+    ylabel("Amplitude")
+    y_fft_1 = fft_eeg(current_d[1], 1)
+    global y_fft_2 = fft_eeg(current_d[2], 1)
+    #ax2.plot(x_fft_1, y_fft_1, color = "red")
+    ax2.plot(abs.(y_fft_2))
+
+
+    ax3 = plt.subplot(133, title = "IFFT (Von FFT rekonstruiert)", sharex = ax1, sharey = ax1)
+    xlabel("Zeit in 5ms")
+    ylabel("Spannungsdifferenz in Mikrovolt")
+    
+    y_ifft_1 = irfft(y_fft_1, 200)
+    y_ifft_2 = irfft(y_fft_2, 200)
+    #ax2.plot(x_fft_1, y_fft_1, color = "red")
+    ax3.plot(y_ifft_2)
+
+end
