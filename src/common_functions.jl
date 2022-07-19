b968eab69c4ca2c950e0d883a03e6a828ff46d3a
AR102 <weltraumkugel@gmail.com>
2022-07-19 19:56:19 +0200

    Missing file necessary for last commit

---
+++ src/common_functions.jl
@@ -0,0 +1,37 @@
+mutable struct ResultHistory
+    train_loss_history::Array{Float64, 1}
+    test_loss_history::Array{Float64, 1}
+    train_accuracy_history::Array{Float64, 1}
+    test_accuracy_history::Array{Float64, 1}
+end
+
+function save_model()
+    bson(SAVE_PATH,
+        model=model,
+        channels=NUM_CHANNELS,
+        frequency=MAX_FREQUENCY,
+        x_history=x_history,
+        train_loss_history=train_loss_history,
+        test_loss_history=test_loss_history,
+        train_accuracy_history=train_accuracy_history,
+        test_accuracy_history=test_accuracy_history)
+end
+
+function load_model()
+    data = BSON.load(LOAD_PATH)
+    if data[:channels] != NUM_CHANNELS
+        NUM_CHANNELS = data[:channels]
+        @warn "Replaced NUM_CHANNELS in config by NUM_CHANNELS used for loaded model since they don't match."
+    end
+    if data[:frequency] != MAX_FREQUENCY
+        MAX_FREQUENCY = data[:frequency]
+        @warn "Replaced MAX_FREQUENCY in config by MAX_FREQUENCY used for loaded model since they don't match."
+    end
+    
+    global x_history=x_history
+    global train_loss_history = train_loss_history
+    global test_loss_history = test_loss_history
+    global train_accuracy_history = train_accuracy_history
+    global test_accuracy_history = test_accuracy_history
+    global model = model
+end
