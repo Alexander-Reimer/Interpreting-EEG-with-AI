786e86678450b972350742f929f15e3db4ca6a86
AR102 <weltraumkugel@gmail.com>
2022-12-03 17:38:59 +0100

    Tested tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using BCIInterface
 using Test
 
 @testset "BCIInterface.jl" begin
+    function test1()
+        if func(5) != 11
+            throw(AssertionError("Doesn't work!!"))
+        end
+    end
+    test1()
     # Write your tests here.
 end
