0487187d8412c14cc2541ce7a320508ec825d78a
AR102 <weltraumkugel@gmail.com>
2023-01-03 15:33:40 +0100

    Added Package LSL for GangionGUI

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,6 +18,12 @@ function test_lengths(lengths, expected_len)
     return true
 end
 
+TEST_DIR = "testdata/"
+TEST_NAME = "Test"
+if isdir(TEST_DIR)
+    rm(TEST_DIR, recursive=true)
+end
+
 @testset "BCIInterface.jl" begin
     @testset "EEG-Devices" begin
         function creation_tests()
@@ -42,34 +48,72 @@ end
         end
     end
     @testset "Experiment" begin
-        function creation_tests(device)
-            _ = Experiment(device, "Test", tags=["test", "significant"],
-                extra_info=Dict(:delay => 2), path="mydata/")
-            return true
-        end
-        device = Device(MCP3208("/test/offline", 8, max_speed_hz=200, online=false))
-        @test creation_tests(device)
-        global experiment = Experiment(device, "Test", tags=["test", "significant"],
-            extra_info=Dict(:delay => 2), path="testdata/")
+        NUM_CHANNELS = 8
+        NUM_COLS_META = 3 # time, tags, extraInfo
+        EXTRA_INFO = Dict(:delay => 2)
+        TAGS = ["test", "significant"]
+        
+        device = Device(MCP3208("/test/offline", NUM_CHANNELS, max_speed_hz=200, online=false))
+        experiment = Experiment(device, TEST_NAME, tags=TAGS,
+            extra_info=EXTRA_INFO, path=TEST_DIR)
 
-        BCIInterface.EEG.clear!(experiment)
         # set up necessary files etc. to avoid problems with timing
         save_data(experiment)
         # Compiling
-        gather_data!(experiment, 0.1, tags=[:left])
+        gather_data!(experiment, 0.1)
         # Actual execution
-        gather_data!(experiment, 0.1, tags=[:left])
+        gather_data!(experiment, 0.1)
+        # With additional tags
+        gather_data!(experiment, 0.1, tags = [:left, :foo])
+
+        # Test if `getproperty` works
         @test experiment.num_samples == size(experiment.raw_data.df)[1]
-        @test experiment.num_samples > 10 # Test if it added some elements
-        @test size(experiment.raw_data.df)[2] == 3 + 8
-        
+        # Test if it added some elements
+        @test experiment.num_samples > 10
+        # Test if number of cols is correct
+        @test size(experiment.raw_data.df)[2] == NUM_CHANNELS + NUM_COLS_META
+
+        # save number of samples (in seperate var because of later clearing)
+        total_num = experiment.num_samples
+
+        # clear all data of experiment
         BCIInterface.EEG.clear!(experiment)
-        gather_data!(experiment, 0.1, tags=[:left], save=false)
+        @test experiment.num_samples == 0
+        @test size(experiment.raw_data.df)[1] == 0
+
+        # without saving in real-time
+        gather_data!(experiment, 0.1, save = false)
         @test experiment.num_samples == size(experiment.raw_data.df)[1]
         @test experiment.num_samples > 1000 # Test if it added some elements
-        @test size(experiment.raw_data.df)[2] == 3 + 8
+
+        total_num += experiment.num_samples
+        # now save the unsaved data
         save_data(experiment)
-        gather_data!(experiment, 0.1, tags=[:left])
+
+        
+        # load saved data into `data`
+        data = load_data(TEST_DIR * TEST_NAME, "RawData", exact_num=true)
+        # check if metadata is correct
+        @test data.num_samples == size(data.df, 1)
+
+        # direct, manual load
+        df = BCIInterface.EEG.CSV.read("testdata/Test/RawData.csv", BCIInterface.EEG.DataFrame; skipto = 2)
+        @test df == data.df
+
+        # create new, empty experiment
+        experiment2 = Experiment(device, TEST_NAME, tags=["test", "significant"],
+            extra_info=Dict(:delay => 2), path=TEST_DIR)
+        # load saved data into it
+        load_data!(experiment2, exact_num=true)
+        # check if metadata is correct
+        @test experiment2.raw_data.num_samples == size(experiment2.raw_data.df, 1)
+
+        # compare data of `data` with data of `experiment2`
+        @test data.df == experiment2.raw_data.df
+        # check if number of collected samples equals number of loaded samples
+        @test total_num == data.num_samples
+        @test total_num == experiment2.raw_data.num_samples
+
     end
     # @testset "Data Saving & Loading"
     @testset "Data processing" begin
