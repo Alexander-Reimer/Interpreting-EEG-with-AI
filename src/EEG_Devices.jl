d3638c6624e36a9cbe83ee624c44b62cbf34b16e
AR102 <weltraumkugel@gmail.com>
2023-01-22 21:25:42 +0100

    Added docstrings

--- src/EEG_Devices.jl
+++ src/EEG_Devices.jl
@@ -7,14 +7,30 @@ struct FFTDataDescriptor <: DataDescriptor
     sample_width::Int
 end
 
+"""
+    FFTDataDescriptor(num_channels::Int, max_freq::Int)
+
+Data descriptor for FFT data in the format of a 2D-Array per sample, with the first dimension corresponding to the frequncies from 1 to max_freq 
+and the seconds dimension corresponding to the channels from 1 to num_channels.
+"""
+FFTDataDescriptor(num_channels::Int, max_freq::Int) = FFTDataDescriptor(num_channels, max_freq, num_channels * max_freq)
+
 struct RawDataDescriptor <: DataDescriptor
     num_channels::Int
     sample_width::Int
 end
 
+"""
+    RawDataDescriptor(num_channels::Int)
+
+Data descriptor for raw data in the format of a 1D-Array per sample, with each channels voltage in a row.
+
+`num_channels::Int`: Number of channels the data has (= width of the row).
+"""
 RawDataDescriptor(num_channels::Int) = RawDataDescriptor(num_channels, num_channels)
 
 abstract type EEGBoard end
+
 #===================#
 #       DEVICES     #
 #===================#
@@ -31,14 +47,17 @@ end
     MCP3208(path::String, num_channels::Int; max_speed_hz::Int=1000, 
     online=true)
 
-Initialise a MCP3208-based self-built EEG. `path` refers to the SPI 
-path of the device and `num_channels` to the number of connected 
-electrodes.
+Initialise a MCP3208-based self-built EEG. 
 
-With `online` set to `true`, the EEG will be "simulated": `get_voltage(...)` 
-will return random values.
+`path::String`: SPI path of the device
 
-TODO: max_speed_hz
+`num_channels::Int`: Number of connected electrodes.
+
+`max_speed_hz::Int`: 1000 by default. Tells the Raspberry Pi (or similar device) how often per second the output should be read. 
+# TODO: explain (side) effects
+
+`online::Bool`: `true` by default; if set to `false`, `get_voltage(...)` will just return 0. 
+This is for testing all associated functions and devices using this board without having it connected (e.g. in tests).
 """
 function MCP3208(path::String, num_channels::Int; max_speed_hz::Int=1000,
     online=true)
@@ -50,6 +69,13 @@ function MCP3208(path::String, num_channels::Int; max_speed_hz::Int=1000,
     return MCP3208(id, num_channels, online, RawDataDescriptor(num_channels), Array{Float64,1}(undef, num_channels))
 end
 
+"""
+    get_voltage(board::MCP3208, channel::Int)
+
+Read digital output of the ADC on channel `channel`.
+
+If MCP3208 was given `online=false` at creation, return 0 instead.
+"""
 function get_voltage(board::MCP3208, channel::Int)
     if channel < 1 || channel > board.num_channels
         throw(ArgumentError("Given channel smaller than 0 or 
@@ -88,10 +114,9 @@ function GanglionGUI(num_channels::Int)
         channel_start = max_freq * (i - 1) + 1
         # ^^                                         ends
         channel_end = max_freq * i
-        timestamp, sample[channel_start:channel_end] = pull_sample(inlet, timeout = 3)
+        timestamp, sample[channel_start:channel_end] = pull_sample(inlet, timeout=3)
     end
-    data_descriptor = FFTDataDescriptor(num_channels, max_freq,
-        num_channels * max_freq)
+    data_descriptor = FFTDataDescriptor(num_channels, max_freq)
     return GanglionGUI(inlet, sample, data_descriptor)
 end
 
