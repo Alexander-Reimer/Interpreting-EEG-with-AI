def4ea737a82a94d660d5ebbd663dece0f334f7a
Alexander Reimer <alexander.reimer2357@gmail.com>
2024-01-29 12:42:48 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(BCIInterface, :DocTestSetup, :(using BCIInterface); recursiv
 makedocs(;
     modules=[BCIInterface],
     authors="Alexander Reimer <alexander.reimer2357@gmail.com>, Matteo Friedrich <matteo.r.friedrich@gmail.com>",
-    repo="https://github.com/AR102/Interpreting-EEG-with-AI/blob/{commit}{path}#{line}",
+    repo="https://github.com/Alexander-Reimer/Interpreting-EEG-with-AI/blob/{commit}{path}#{line}",
     sitename="BCIInterface.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://AR102.github.io/Interpreting-EEG-with-AI",
+        canonical="https://Alexander-Reimer.github.io/Interpreting-EEG-with-AI",
         edit_link="main",
         assets=String[]
     ),
@@ -23,6 +23,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/AR102/Interpreting-EEG-with-AI",
+    repo="github.com/Alexander-Reimer/Interpreting-EEG-with-AI",
     devbranch="refactor"
 )
